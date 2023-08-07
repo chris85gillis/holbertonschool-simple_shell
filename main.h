12e49df32a900a0a59b9faf43e41b28bff3cfbf7
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-08-07 02:40:24 -0500

    Update main.h
    added in function prototypes

--- main.h
+++ main.h
@@ -10,5 +10,11 @@
 
 void execmd(char **argv);
 char *get_location(char *command);
+char *read_command(void);
+char **tokenize_command(char *lineptr);
+int execmd(char **argv);
+char *duplicate_string(const char *str);
+char *build_file_path(const char *path, const char *command);
+char *find_command_location(const char *path, const char *command);
 
 #endif
