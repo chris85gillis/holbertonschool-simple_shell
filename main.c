1ed23e163f978fa90a49a9d87b75ba1fd23c8a87
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-08-07 03:06:26 -0500

    Update main.c
    changes didn't work reverting to old code

--- main.c
+++ main.c
@@ -1,105 +1,83 @@
 #include "main.h"
 
 /**
- * read_command - Read a command from the user.
+ * get_location -  find the path of a given command.
+ * @command: the name of the command for which you want to find the full path.
  *
- * Return: The entered command.
+ * Return: NULL
  */
-char *read_command(void)
+
+char *get_location(char *command)
 {
-	char *lineptr = NULL;
-	size_t n = 0;
-	ssize_t nchars_read;
+	char *path, *path_copy, *path_token, *file_path;
+	int command_length, directory_length;
+	struct stat buffer;
 
-	printf("(SimShell) $ ");
-	nchars_read = getline(&lineptr, &n, stdin);
+	path = getenv("PATH");
 
-	if (nchars_read == -1)
+	if (path)
 	{
-		printf("Exiting shell....\n");
-		free(lineptr);
+		/*Duplicate the path string -> remember to free up */
+		/*memory for this because strdup allocates memory that */
+		/*needs to be freed*/
+		path_copy = strdup(path);
+		/*Get length of the command that was passed */
+		command_length = strlen(command);
+
+		/*Let's break down the path variable and get all the */
+		/*directories available*/
+		path_token = strtok(path_copy, ":");
+
+		while (path_token != NULL)
+		{
+			/*Get the length of the directory*/
+			directory_length = strlen(path_token);
+			/*allocate memory for storing the command name */
+			/*together with the directory name */
+			file_path = malloc(command_length + directory_length + 2);
+			/*NB: we added 2 for the slash and null character */
+			/*we will introduce in the full command */
+
+			/*to build the path for the command, let's copy the */
+			/*directory path and concatenate the command to it */
+			strcpy(file_path, path_token);
+			strcat(file_path, "/");
+			strcat(file_path, command);
+			strcat(file_path, "\0");
+
+			/*let's test if this file path actually exists */
+			/*and return it if it does, otherwise try the next */
+			/*directory */
+			if (stat(file_path, &buffer) == 0)
+			/*return value of 0 means success implying that */
+			/*the file_path is valid*/
+			{
+				/*free up allocated memory before returning */
+				/*your file_path */
+				free(path_copy);
+
+				return (file_path);
+			}
+			else
+			{
+				/*free up the file_path memory so we can check*/
+				/*for another path*/
+				free(file_path);
+				path_token = strtok(NULL, ":");
+			}
+		}
+		/*if we don't get any file_path that exists for */
+		/*the command, we return NULL but we need to free up */
+		/*memory for path_copy */
+		free(path_copy);
+
+		/*before we exit without luck, let's see */
+		/*if the command itself is a file_path that exists */
+		if (stat(command, &buffer) == 0)
+		{
+			return (command);
+		}
 		return (NULL);
 	}
-
-	return (lineptr);
-}
-
-/**
- * tokenize_command - Tokenize a command string into arguments.
- * @command: The command string to tokenize.
- *
- * Return: An array of argument strings.
- */
-char **tokenize_command(char *command)
-{
-	const char *delim = " \n";
-	int num_tokens = 0;
-	char *token;
-	char **cmd_argv;
-	int i = 0;
-
-	token = strtok(command, delim);
-
-	while (token != NULL)
-	{
-		num_tokens++;
-		token = strtok(NULL, delim);
-	}
-	num_tokens++;
-
-	cmd_argv = malloc(sizeof(char *) * num_tokens);
-
-	token = strtok(command, delim);
-
-	for (i = 0; token != NULL; i++)
-	{
-		cmd_argv[i] = strdup(token);
-		token = strtok(NULL, delim);
-	}
-	cmd_argv[i] = NULL;
-
-	return (cmd_argv);
-}
-
-/**
- * main - Entry point.
- * @ac: Number of command-line arguments passed to the program.
- * @cmd_argv: The command-line arguments provided to the program.
- *
- * Return: 0
- */
-int main(int ac, char **cmd_argv)
-{
-	char *command;
-
-	(void)ac;
-
-	while (1)
-	{
-		command = read_command();
-
-		if (command == NULL)
-		break;
-
-		cmd_argv = tokenize_command(command);
-
-		if (cmd_argv == NULL)
-		{
-			free(command);
-			continue;
-		}
-
-		if (strcmp(cmd_argv[0], "exit") == 0)
-		{
-			printf("Exiting shell....\n");
-			break;
-		}
-
-		execmd(cmd_argv);
-
-		free(command);
-		free(cmd_argv);
-	}
-
-	return (0);
+	return (NULL);
 }
