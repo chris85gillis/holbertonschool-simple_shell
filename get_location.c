cd747a6945d0763501b9f80f6c673dfe4bd2e34e
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-08-07 02:33:05 -0500

    Update get_location.c

--- get_location.c
+++ get_location.c
@@ -44,15 +44,14 @@ char *build_file_path(const char *path, const char *command)
 char *find_command_location(const char *path, const char *command)
 {
 	const char *delim = ":";
-	struct stat buffer;
 	char *path_copy = duplicate_string(path);
-	if (!path_copy)
-		return NULL;
+	char *file_path;
+	 if (!path_copy)
+        return NULL;
 	
+	struct stat buffer;
 	char *path_token = strtok(path_copy, delim);
 	char *file_path;
-	
-	
 
 	while (path_token)
 	{
